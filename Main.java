d1f39eaab7cee36df6c2afdd1c1e6b08cf18fcbf
nakajima913 <22j5104@stu.meisei-u.ac.jp>
2024-12-13 13:30:00 +0900

    Add files via upload
    変更後のファイル

--- Main.java
+++ Main.java
@@ -3,11 +3,7 @@ package jp.ac.meiseiu.stu.j225104.Kari;
 public class Main {
 
 	public static void main(String[] args) {
-		public static void main(String[] args) {
-			Frame frame = new Frame();
-			frame.publicMethod();
-		}
-
+		new Frame();
 	}
 
 }
